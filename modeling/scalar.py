0f09149a9bf1e5c1c85df21c60d258f52164e0a0
YayaToure97 <yayasangbapletoure@id.uff.br>
2024-01-26 19:58:58 -0300

    in-depth velocity 1D model

--- modeling/scalar.py
+++ modeling/scalar.py
@@ -12,35 +12,71 @@ class Wavefield_1D():
         self.nt = 1001
         self.dt = 1e-3
         self.fmax = 30.0
+        self.nz=1001 # Número total de pontos
+        self.dz=10   # Espaçamentos entre os pontos
+        
+        self.interfaces= np.array([1000,1500,2000,2500]) # definições das interfaces
+        self.velocidades= np.array([1500,2000,2500,3000,3500]) # definições das velocidades
+
+        self.depth = np.linspace(0,self.dz*self.nz,self.nz,endpoint=False) # definição da profundidade 
+        #self.depth = np.arange(self.nz)*self.nz
+        self.model =   self.velocidades[0]*np.zeros(self.nz) ### criação de um modelo com  matriz vazia
+    
+    def set_model(self):
+        pass 
+
+        for i in range(len(self.interfaces)):
+            self.model[int(self.interfaces[i] / self.dz):] = self.velocidades[i+1] 
+    
+
+    def plot_model(self):
+        pass 
+
+        fig,ax = plt.subplots(num = 'model plot', figsize=(4,8),clear=True)
+        
+        ax.plot(self.model,self.depth)
+        ax.set_title('model plot',fontsize=18)
+        ax.set_xlabel('Velocity [m/s]',fontsize=15)
+        ax.set_ylabel('depth [m]',fontsize =15) 
+
+        ax.set_ylim([0,(self.nz-1)*self.dz])
+
+        ax.invert_yaxis()
+        fig.tight_layout()
+        plt.show()
+            
 
     def get_type(self):
-        print(self._type)
+         print(self._type)
 
     def set_wavelet(self):
     
-        t0 = 2.0*np.pi/self.fmax
-        fc = self.fmax/(3.0*np.sqrt(np.pi))
-        td = np.arange(self.nt)*self.dt - t0
+         t0 = 2.0*np.pi/self.fmax
+         fc = self.fmax/(3.0*np.sqrt(np.pi))
+         td = np.arange(self.nt)*self.dt - t0
 
-        arg = np.pi*(np.pi*fc*td)**2.0
+         arg = np.pi*(np.pi*fc*td)**2.0
 
-        self.wavelet = (1.0 - 2.0*arg)*np.exp(-arg)
+         self.wavelet = (1.0 - 2.0*arg)*np.exp(-arg)
 
     def plot_wavelet(self):
        
-        t = np.arange(self.nt)*self.dt
+         t = np.arange(self.nt)*self.dt
 
-        fig, ax = plt.subplots(figsize = (10, 5), clear = True)
+         fig, ax = plt.subplots(figsize = (10, 5), clear = True)
 
-        ax.plot(t, self.wavelet)
-        ax.set_title("Wavelet", fontsize = 18)
-        ax.set_xlabel("Time [s]", fontsize = 15)
-        ax.set_ylabel("Amplitude", fontsize = 15) 
+         ax.plot(t, self.wavelet)
+         ax.set_title("Wavelet", fontsize = 18)
+         ax.set_xlabel("Time [s]", fontsize = 15)
+         ax.set_ylabel("Amplitude", fontsize = 15) 
         
-        ax.set_xlim([0, np.max(t)])
+         ax.set_xlim([0, np.max(t)])
         
-        fig.tight_layout()
-        plt.show()
+         fig.tight_layout()
+         plt.show() 
+
+
+    
 
 
 class Wavefield_2D():
@@ -51,30 +87,30 @@ class Wavefield_2D():
         self._type = "2D wave propagation in constant density acoustic isotropic media"    
 
 # Read the parameters from file
-self.nx=200
-self.nz=50
+# self.nx=200
+# self.nz=50
 
-self.model=np.zeros(nz)
-self.depth=np.arange(nz)*self.nx
+# self.model=np.zeros(nz)
+# self.depth=np.arange(nz)*self.nx
 
-self.deep =[1500,1800,2000,2200,2500]
-self.Density =[1000,2100,2150,2180,2200]
-self.interfaces =[100,150,200,300] 
+# self.deep =[1500,1800,2000,2200,2500]
+# self.Density =[1000,2100,2150,2180,2200]
+# self.interfaces =[100,150,200,300] 
 
-Vp=np.zeros((nx,nz))
-Rho=np.zeros((nx,nz)) 
+# Vp=np.zeros((nx,nz))
+# Rho=np.zeros((nx,nz)) 
 
 
-for i in range(len(interfaces)):
-    vp[int(interfaces[i] / dh):] = velocidades[i+1]
-    rho[int(interfaces[i] / dh):] = densidades[i+1]
+# for i in range(len(interfaces)):
+#     vp[int(interfaces[i] / dh):] = velocidades[i+1]
+#     rho[int(interfaces[i] / dh):] = densidades[i+1]
 
-plt.imshow(vp*rho)
-yaya=plt.colorbar(orientation ='horizontal')
-plt.xlabel('Distância')
-plt.ylabel('Profundidade')
-yaya.set_label('impedância')
-plt.show()
+# plt.imshow(self.vp*self.rho)
+# yaya=plt.colorbar(orientation ='horizontal')
+# plt.xlabel('Distância')
+# plt.ylabel('Profundidade')
+# yaya.set_label('impedância')
+# plt.show()
 
 
 class Wavefield_3D(Wavefield_2D):
--- simulation.py
+++ simulation.py
@@ -12,7 +12,10 @@ def simulation():
     myWave[id].get_type()
 
     myWave[id].set_wavelet()
-    myWave[id].plot_wavelet()
+    myWave[id].plot_wavelet() 
+
+    myWave[id].set_model()
+    myWave[id].plot_model()
 
 
 if __name__ == "__main__":
